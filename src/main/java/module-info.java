ae449ab3e8b30a7c41cb96cfb92b96a23be91758
Alex <29205589+aquino-a@users.noreply.github.com>
2023-05-26 11:39:27 +0900

    remove module

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module followme {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.annotation;
-    requires afterburner.fx;
-
-    opens com.airhacks.followme to javafx.graphics;
-  }
